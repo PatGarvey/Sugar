ee39af1f75ed0b432f94df4f2126cc251a30d49c
Kristian Arlt <kristian.arlt@gmail.com>
2015-11-12 17:51:32 +0100

    dynamic loading

--- shugarcrm/js/sugar-versions-controller.js
+++ shugarcrm/js/sugar-versions-controller.js
@@ -3,10 +3,14 @@ var path = window.location.href.replace(/^https?:\/\/[^\/]+\//i, "").replace(/\/
 var edition = "Ultimate";
 var version = "7.6";
 
+	
 if( path == "Documentation"){
-	$(".content-heading").append(editionVersions);
+  $(".content-heading").append(editionVersions());
 	var url = "/Documentation/Sugar_Versions/"+version+"/"+Utils.getAbbreviatedEdition(edition)+"/";
-	loadEditionVersion(url);
+	// loadEditionVersion(url);
+
+
+
 
 }
 if(path.split("/").length == 4)
@@ -57,24 +61,57 @@ function loadEditionVersion(url){
    	});
 }
 
+//Load Editions & Versions from API
+var addVersions = function(parent){
+  $.getJSON( "http://scarlett.sugarcrm.com/public/index.php/api/v1/versions", function( data ) {
+    var items = [];
+    $.each( data, function( key, val ) {
+      items.push( '<button type="button" class="btn btn-default active">' + val + "</button>" );
+    });
+   
+    var div = $( "<div />", {
+      "class": "btn-group btn-group-sm",
+      "role":"groupEdition",
+      "id":"groupVersion",
+      html: items.join( "" )
+    }).appendTo(parent);
 
-var editionVersions = '<section class="active-filters">'+
-     '<div>'+
-      '   <label>Edition:</label>'+
-      '   <div class="btn-group btn-group-sm" role="groupEdition" id="groupEdition">'+
-      '     <button type="button" class="btn btn-default active">Ultimate</button>'+
-      '    	<button type="button" class="btn btn-default">Enterprise</button>'+
-      '     <button type="button" class="btn btn-default">Corporate</button>'+
-      '     <button type="button" class="btn btn-default">Professional</button>'+
-      '     <button type="button" class="btn btn-default">Community Edition</button>'+
-      '   </div>'+
-      '   &nbsp;'+
-      '   <label>Version:</label>'+
-      '   <div class="btn-group btn-group-sm" role="groupVersion" id="groupVersion">'+
-      '     <button type="button" class="btn btn-default active">7.6</button>'+
-      '     <button type="button" class="btn btn-default">7.5</button>'+
-      '     <button type="button" class="btn btn-default">6.7</button>'+
-      '     <button type="button" class="btn btn-default">6.5</button>'+
-      '   </div>'+
-     '</div>  '+
-   '</section>';
+    return div;
+  });
+}
+
+var createButtons = function(data){
+  var div = document.createElement('div');
+  $.each(data, function( index, value ) {
+    // alert( index + ": " +  );
+    $(div).append('<button type="button" class="btn btn-default active">'+value+'</button>');
+  });
+  
+
+}
+var versions = $('<section class="active-filters"><div></div></section>');
+$(".content-body").html("");
+$(".content-body").append(versions);
+addVersions(versions);
+
+
+// var editionVersions = '<section class="active-filters">'+
+//      '<div>'+
+//       '   <label>Edition:</label>'+
+//       '   <div class="btn-group btn-group-sm" role="groupEdition" id="groupEdition">'+
+//       '     <button type="button" class="btn btn-default active">Ultimate</button>'+
+//       '    	<button type="button" class="btn btn-default">Enterprise</button>'+
+//       '     <button type="button" class="btn btn-default">Corporate</button>'+
+//       '     <button type="button" class="btn btn-default">Professional</button>'+
+//       '     <button type="button" class="btn btn-default">Community Edition</button>'+
+//       '   </div>'+
+//       '   &nbsp;'+
+//       '   <label>Version:</label>'+
+//       '   <div class="btn-group btn-group-sm" role="groupVersion" id="groupVersion">'+
+//       '     <button type="button" class="btn btn-default active">7.6</button>'+
+//       '     <button type="button" class="btn btn-default">7.5</button>'+
+//       '     <button type="button" class="btn btn-default">6.7</button>'+
+//       '     <button type="button" class="btn btn-default">6.5</button>'+
+//       '   </div>'+
+//      '</div>  '+
+//    '</section>';
