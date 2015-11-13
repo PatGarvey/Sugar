2d02eb16c20cd750131aefb95212eef345423dbe
Kristian Arlt <kristian.arlt@gmail.com>
2015-11-13 15:14:11 +0100

    fix dem all

--- shugarcrm/js/sugar-versions-controller.js
+++ shugarcrm/js/sugar-versions-controller.js
@@ -1,59 +1,52 @@
-
 var path = window.location.href.replace(/^https?:\/\/[^\/]+\//i, "").replace(/\/$/, "");
 var edition = "Ultimate";
 var version = "7.6";
 
 //Button Bar CLICK - ALL
-$(".btn-group > .btn").click(function(){
-	if($(this).hasClass("disabled"))
-		return;
+$(".btn-group > .btn").click(function() {
+  if ($(this).hasClass("disabled"))
+    return;
   $(this).addClass("active").siblings().removeClass("active");
   $("#okButton").addClass("btn-primary");
 
-    //Only for demo
-    $("#sugar-on-ultimate .row").toggleClass("hidden");
+  //Only for demo
+  $("#sugar-on-ultimate .row").toggleClass("hidden");
 
-  });
+});
 
-function loadEditionVersion(version, edition){
-	// $( ".content-children" ).load( url + " .content-body", function() {
-  		// Utils.transformTableToDivs();
-    // });
+function loadEditionVersion(version, edition) {
 
-  $(".content-children").html("");
+  $(".content-section").html("");
 
 
-  $.getJSON("http://scarlett.sugarcrm.com/public/index.php/api/v1/documentation?version="+version+"&edition="+Utils.getAbbreviatedEdition(edition), function(data){
+  $.getJSON("http://scarlett.sugarcrm.com/public/index.php/api/v1/documentation?version=" + version + "&edition=" + Utils.getAbbreviatedEdition(edition), function(data) {
     var cols = [];
-    $.each( data, function( key, val ) {
+    $.each(data, function(key, val) {
       var items = [];
-      $.each(val, function( key, val){
-        if(val.lastIndexOf("/") == val.length - 1)
+      $.each(val, function(key, val) {
+        if (val.lastIndexOf("/") == val.length - 1)
           val = val.substring(0, val.length - 1);
-        items.push("<li><a href="+val+">"+key+"</a></li>");
+        items.push("<li><a href=" + val + ">" + key + "</a></li>");
       });
       var ul = $("<ul />", {
-        "class":"plain-list",
-        html : items.join("")
+        "class": "plain-list",
+        html: items.join("")
       });
 
       var holder = $('<div>').append(ul);
 
-      cols.push( '<div class="col-sm-6 col-md-3 content-col"><h2>'+key+'</h2>'+holder.html()+'</div>' );
+      cols.push('<div class="col-sm-6 col-md-3 content-col"><h2>' + key + '</h2>' + holder.html() + '</div>');
     });
 
-    var div = $( "<div />", {
+    var div = $("<div />", {
       "class": "row",
-      "id":"columns",
-      html: cols.join( "" )
-    }).appendTo($(".content-children"));
+      "id": "columns",
+      html: cols.join("")
+    }).appendTo($(".content-section"));
     // <div class="col-sm-6 col-md-3 content-col">
     //                       <h2>User Guides</h2>
     //                       <ul class="plain-list">
     //                         <li><a href="">Application</a></li>
-    //                         <li><a href="">Application Guide Portal</a></li>
-    //                         <li><a href="">Portal User Guide Plug-ins</a></li>
-    //                         <li><a href="">Plug-ins Documentation Mobile</a></li>
     //                         <li><a href="">SugarCRM Mobile</a></li>
     //                       </ul>
     //                     </div>
@@ -61,88 +54,94 @@ function loadEditionVersion(version, edition){
 }
 
 //Load Editions & Versions from API
-var addVersions = function(parent){
-  $.getJSON( "http://scarlett.sugarcrm.com/public/index.php/api/v1/versions", function( data ) {
+var addVersions = function(parent) {
+  $.getJSON("http://scarlett.sugarcrm.com/public/index.php/api/v1/versions", function(data) {
     var items = [];
-    $.each( data, function( key, val ) {
-      items.push( '<button type="button" class="btn btn-default">' + val + "</button>" );
+    $.each(data, function(key, val) {
+      items.push('<button type="button" class="btn btn-default">' + val + "</button>");
     });
 
-    var div = $( "<div />", {
+    var div = $("<div />", {
       "class": "btn-group btn-group-sm",
-      "role":"groupVersion",
-      "id":"groupVersion",
-      html: items.join( "" )
+      "role": "groupVersion",
+      "id": "groupVersion",
+      html: items.join("")
     }).appendTo(parent);
 
-    // if(window.location.href.indexOf("Documentation/Sugar_Versions/")>-1){
     //Version Button Bar CLICK
-    $("#groupVersion > .btn").click(function(){
-      if($(this).hasClass("disabled"))
+    $("#groupVersion > .btn").click(function() {
+      if ($(this).hasClass("disabled"))
         return;
       $(this).addClass("active").siblings().removeClass("active");
       version = $(this).html();
-      $("#editionTitle").html(version+" "+edition);
-        getEditions(parent, version);
+      $("#editionTitle").html(version + " " + edition);
+      getEditions(parent, version);
     });
 
     return div;
   });
 }
 
-var getEditions = function(parent, version){
-   $.getJSON( "http://scarlett.sugarcrm.com/public/index.php/api/v1/editions/"+version, function( data ) {
-        var items = [];
-        $.each( data, function( key, val ) {
-          items.push( '<button type="button" class="btn btn-default">' + val + "</button>" );
-        });
+var getEditions = function(parent, version) {
+  $.getJSON("http://scarlett.sugarcrm.com/public/index.php/api/v1/editions/" + version, function(data) {
+    var items = [];
+    $.each(data, function(key, val) {
+      items.push('<button type="button" class="btn btn-default">' + val + "</button>");
+    });
 
-        var div = $( "<div />", {
-          "class": "btn-group btn-group-sm",
-          "role":"groupEdition",
-          "id":"groupEdition",
-          html: items.join( "" )
-        }).appendTo(parent);
+    var div = $("#groupEdition");
+    if (div.length == 0) {
+      var label = $("<span class='m-l-sm m-r-xs'>Edition:</span>").appendTo(parent);
 
-        //Edition Button Bar CLICK
-        $("#groupEdition > .btn").click(function(){
-          $(this).addClass("active").siblings().removeClass("active");
-          edition = $(this).html();
-          if(edition == "Community Edition"){
-           version = "6.5";
-           $("#groupVersion > .btn").removeClass("active");
-           $("#groupVersion button:nth-child(4)").addClass("active").siblings().addClass("disabled");
-           }else{
-             $("#groupVersion > .btn").removeClass("disabled");
-           }
+      div = $("<div />", {
+        "class": "btn-group btn-group-sm",
+        "role": "groupEdition",
+        "id": "groupEdition",
+        html: items.join("")
+      }).appendTo(parent);
+    }else{
+      //replace buttons
+      $("#groupEdition > button").remove();
+      $("#groupEdition").append(items.join(""));
 
-           $("#editionTitle").html(version+" "+edition);
-           var url = "/Documentation/Sugar_Versions/"+version+"/"+Utils.getAbbreviatedEdition(edition)+"/";
-           loadEditionVersion(version, edition);
-        });
+    }
 
-      });
+    //Edition Button Bar CLICK
+    $("#groupEdition > .btn").click(function() {
+      $(this).addClass("active").siblings().removeClass("active");
+      edition = $(this).html();
+      if (edition == "Community Edition") {
+        version = "6.5";
+        $("#groupVersion > .btn").removeClass("active");
+        $("#groupVersion button:nth-child(4)").addClass("active").siblings().addClass("disabled");
+      } else {
+        $("#groupVersion > .btn").removeClass("disabled");
+      }
+
+      $("#editionTitle").html(version + " " + edition);
+      var url = "/Documentation/Sugar_Versions/" + version + "/" + Utils.getAbbreviatedEdition(edition) + "/";
+      loadEditionVersion(version, edition);
+    });
+
+  });
 }
 
 
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-  if( path == "Documentation"){
+  if (path == "Documentation" || path.indexOf("file:") > -1) {
     // $(".content-heading").append(editionVersions());
-    var url = "/Documentation/Sugar_Versions/"+version+"/"+Utils.getAbbreviatedEdition(edition)+"/";
-    var versions = $('<section class="active-filters"><div><label>Edition:</label></div></section><div class="content-children"></div>');
+    var url = "/Documentation/Sugar_Versions/" + version + "/" + Utils.getAbbreviatedEdition(edition) + "/";
+    var versions = $('<section class="active-filters"><div><span class="m-r-xs">Version:</span></div></section><div class="content-section"></div>');
     $(".content-body").html("");
     $(".content-body").append(versions);
     addVersions($(".active-filters > div"));
 
   }
-  if(path.split("/").length == 4)
-    Utils.transformTableToDivs();   
+  if (path.split("/").length == 4)
+    Utils.transformTableToDivs();
 });
-
-
-
 
 
 
