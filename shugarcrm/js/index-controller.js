e81168eff662678b272849c9ce3aae255b11636c
Kristian Arlt <kristian.arlt@gmail.com>
2015-11-20 12:37:33 +0100

    dynamic index feed

--- shugarcrm/js/index-controller.js
+++ shugarcrm/js/index-controller.js
@@ -2,16 +2,33 @@
 
 $(document).ready(function() {
 
-	var latestPages;
-	var popularPages;
 	//Get Last Modified pages
 	$.getJSON('http://scarlett.sugarcrm.com/public/api/v1/search?q=*&sort=modified:desc&size=10', function(json, textStatus) {
-		latestPages = json;
+		//TODO add #id
+		var list = $("section.content-section > div.row > div:nth-child(3)");
+		list.html("");
+		list.append("<h2>Latest News & Updates</h2>");
+		for (var i = 0; i < 4; i++) {
+			var row = json.data[i];
+			list.append("<h3><a href='"+row.url+"'>"+row.title+"</a></h3>");
+			list.append("<p>"+row.summary+"</p>");
+		}
+
+
 	});
 	//Get Most Popular pages
 	$.getJSON('http://scarlett.sugarcrm.com/public/api/v1/search?q=*&sort=rating:desc&size=10', function(json, textStatus) {
-		popularPages = json;
+
+		//TODO add #id
+		var list = $("section.content-section > div.row > div:nth-child(2)");
+		list.html("");
+		list.append("<h2>Popular Topics</h2>");
+		for (var i = 0; i < 4; i++) {
+			var row = json.data[i];
+			list.append("<h3><a href='"+row.url+"'>"+row.title+"</a></h3>");
+			list.append("<p>"+row.summary+"</p>");
+		}
 	});
 
 
-}
+});
