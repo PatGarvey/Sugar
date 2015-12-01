db6d256907fd37140be173e9ecb2f4a631684a22
Kristian Arlt <kristian.arlt@gmail.com>
2015-12-01 11:54:08 +0100

    save search dropdown all the time

--- shugarcrm/js/search.js
+++ shugarcrm/js/search.js
@@ -122,11 +122,11 @@ var Search = (function() {
 
     //Save Criteria to Local Storage
     if (window.localStorage) {
-      if (criteria[1].length > 6)
+      // if (criteria[1].length > 6)
         window.localStorage.setItem("edition", criteria[1].substr(5));
-      if (criteria[2].length > 6)
+      // if (criteria[2].length > 6)
         window.localStorage.setItem("version", criteria[2].substr(5));
-      if (criteria[3].length > 6)
+      // if (criteria[3].length > 6)
         window.localStorage.setItem("usertype", criteria[3].substr(5));
     }
 
