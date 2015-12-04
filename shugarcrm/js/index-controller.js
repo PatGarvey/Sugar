075c561c920f5445ea733bb436bf9274e2219586
Kristian Arlt <kristian.arlt@gmail.com>
2015-12-04 15:37:51 +0100

    home page 3rd column change

--- shugarcrm/js/index-controller.js
+++ shugarcrm/js/index-controller.js
@@ -9,11 +9,11 @@ $(document).ready(function() {
 	//    }
 	//  });
 
-	//Get Last Modified pages
+
 	$.ajax({
 		type: "GET",
 		crossDomain: true,
-        // headers: { 'Access-Control-Allow-Origin': '*' },
+		// headers: { 'Access-Control-Allow-Origin': '*' },
 		url: "https://community.sugarcrm.com/view-browse-feed.jspa?filterID=all~objecttype~objecttype%5Bblogpost%5D&browseSite=content&userIDs=-1&browseViewID=content",
 		dataType: "text",
 		success: function(xml) {
@@ -27,11 +27,23 @@ $(document).ready(function() {
 				list.append("<p>" + row.summary + "</p>");
 			}
 		},
-		error: function(error){
-			console.log("error "+error);
+		error: function(error) {
+			console.log("error " + error);
 		}
 	});
-	// $.getJSON('http://scarlett.sugarcrm.com/public/api/v1/search?q=*&sort=modified:desc&size=10', function(json, textStatus) {
+
+	//Get Last Modified pages
+	$.getJSON('http://scarlett.sugarcrm.com/public/api/v1/search?q=*&sort=modified:desc&size=10', function(json, textStatus) {
+		//TODO add #id
+		var list = $("section.content-section > div.row > div:nth-child(3)");
+		list.html("");
+		list.append("<h2>Content Updates</h2>");
+		for (var i = 0; i < 4; i++) {
+			var row = json.data[i];
+			list.append("<h3><a href='" + row.url + "'>" + row.title + "</a></h3>");
+			list.append("<p>" + row.summary + "</p>");
+		}
+	});
 
 	//Get Most Popular pages
 	$.getJSON('http://scarlett.sugarcrm.com/public/api/v1/search?q=*&sort=rating:desc&size=10', function(json, textStatus) {
