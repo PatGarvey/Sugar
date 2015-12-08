3683cbb7bb74602e819a33a37ffa1a843f39f225
Kristian Arlt <kristian.arlt@gmail.com>
2015-12-08 18:29:10 +0100

    XML parsing for homepage

--- shugarcrm/js/index-controller.js
+++ shugarcrm/js/index-controller.js
@@ -14,17 +14,23 @@ $(document).ready(function() {
 		type: "GET",
 		crossDomain: true,
 		// headers: { 'Access-Control-Allow-Origin': '*' },
-		url: "https://community.sugarcrm.com/view-browse-feed.jspa?filterID=all~objecttype~objecttype%5Bblogpost%5D&browseSite=content&userIDs=-1&browseViewID=content",
-		dataType: "text",
+		// url: "https://community.sugarcrm.com/view-browse-feed.jspa?filterID=all~objecttype~objecttype%5Bblogpost%5D&browseSite=content&userIDs=-1&browseViewID=content",
+		url: "rssfeed.xml",
+		dataType: "xml",
 		success: function(xml) {
 			//TODO add #id
 			var list = $("section.content-section > div.row > div:nth-child(1)");
+			
+			var channel = xml.children[0].children[0];
+			var items = channel.getElementsByTagName("item");
 			list.html("");
 			list.append("<h2>Latest News</h2>");
 			for (var i = 0; i < 4; i++) {
-				var row = xml.channel[i];
-				list.append("<h3><a href='" + row.url + "'>" + row.title + "</a></h3>");
-				list.append("<p>" + row.summary + "</p>");
+				var row = items[i];
+				var link = row.getElementsByTagName("description")[0].innerHTML;
+				var decoded = $('<div/>').html(link).text();
+				list.append("<h3><a href='" + row.getElementsByTagName("link")[0].innerHTML + "'>" + row.getElementsByTagName("title")[0].innerHTML + "</a></h3>");
+				list.append(decoded);
 			}
 		},
 		error: function(error) {
